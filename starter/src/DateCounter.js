2c5a544ef182961102ea177f62624ca276e11535
Saodat <saodat.riskiyeva@gmail.com>
2025-05-20 17:55:21 -0400

    adding initial state to reducer

--- starter/src/DateCounter.js
+++ starter/src/DateCounter.js
@@ -10,10 +10,14 @@ function reducer(state, action) {
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
+  // const [step, setStep] = useState(1);
+
+  const initialState = {
+    count: 0,
+    step: 1,
+  };
 
   const [count, dispatch] = useReducer(reducer, 0);
-
-  const [step, setStep] = useState(1);
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
