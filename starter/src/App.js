810d8bda0c4aed857d92a9f183af346ef2856de9
Saodat <saodat.riskiyeva@gmail.com>
2025-05-21 22:07:37 -0400

    loading questions on mount by fetching data from a file

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
 export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error"));
+  }, []);
+
   return (
     <div className="app">
       <Header />
